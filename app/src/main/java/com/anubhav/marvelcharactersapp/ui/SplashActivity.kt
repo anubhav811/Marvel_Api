5ac85ca26943a5b709e64045982c3862a6fe626b
Anubhav Chachra <chachraanubhav@gmail.com>
2022-02-02 00:44:14 +0530

    placeholder splash added

--- app/src/main/java/com/anubhav/marvelcharactersapp/ui/SplashActivity.kt
+++ app/src/main/java/com/anubhav/marvelcharactersapp/ui/SplashActivity.kt
@@ -1,12 +1,19 @@
 package com.anubhav.marvelcharactersapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.anubhav.marvelcharactersapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
     }
 }
