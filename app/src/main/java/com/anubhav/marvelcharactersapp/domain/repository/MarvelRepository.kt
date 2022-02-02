fd396aec6bd5e9f464d13754c8a17cbbc7e3de2c
Anubhav Chachra <chachraanubhav@gmail.com>
2022-02-02 12:09:32 +0530

    merged repository and implementation at one place (might change later)

--- app/src/main/java/com/anubhav/marvelcharactersapp/domain/repository/MarvelRepository.kt
+++ app/src/main/java/com/anubhav/marvelcharactersapp/domain/repository/MarvelRepository.kt
@@ -1,8 +1,11 @@
 package com.anubhav.marvelcharactersapp.domain.repository
 
+import com.anubhav.marvelcharactersapp.data.RetrofitInstance
 import com.anubhav.marvelcharactersapp.data.dto.CharacterResponse
 
 interface MarvelRepository {
 
-    suspend fun getAllCharacters(offset:Int):CharacterResponse
+    suspend fun getAllCharacters(offset:Int):CharacterResponse =
+        RetrofitInstance.api.getAllCharacters(offset = 20.toString())
+
 }
