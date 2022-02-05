81e898afb4c9994d76d403dadd4b92768113d4f7
Anubhav Chachra <chachraanubhav@gmail.com>
2022-02-05 23:09:17 +0530

    differ list

--- app/src/main/java/com/anubhav/marvelcharactersapp/ui/CharacterViewModel.kt
+++ app/src/main/java/com/anubhav/marvelcharactersapp/ui/CharacterViewModel.kt
@@ -1,4 +1,36 @@
 package com.anubhav.marvelcharactersapp.ui
 
-class CharacterViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anubhav.marvelcharactersapp.data.dto.CharacterResponse
+import com.anubhav.marvelcharactersapp.domain.repository.MarvelRepository
+import com.anubhav.marvelcharactersapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class CharacterViewModel(
+    val marvelRepository: MarvelRepository
+) : ViewModel() {
+    val characters: MutableLiveData<Resource<CharacterResponse>> = MutableLiveData()
+
+    var characterResponse : CharacterResponse?= null
+
+    fun getCharacters(offset: String) = viewModelScope.launch {
+            characters.postValue(Resource.Loading())
+        val response = marvelRepository.getAllCharacters(20)
+        characters.postValue(handleCharacterResponse(response))
+    }
+
+    private fun handleCharacterResponse(response: Response<CharacterResponse>): Resource<CharacterResponse>? {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                if (characterResponse == null) {
+                    characterResponse = resultResponse
+                }
+                return Resource.Success(characterResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
--- app/src/main/java/com/anubhav/marvelcharactersapp/ui/fragments/CharacterFragment.kt
+++ app/src/main/java/com/anubhav/marvelcharactersapp/ui/fragments/CharacterFragment.kt
@@ -1,17 +1,29 @@
 package com.anubhav.marvelcharactersapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.anubhav.marvelcharactersapp.R
+import com.anubhav.marvelcharactersapp.adapters.CharacterAdapter
 import com.anubhav.marvelcharactersapp.databinding.FragmentCharacterBinding
 import com.anubhav.marvelcharactersapp.databinding.FragmentHomeBinding
+import com.anubhav.marvelcharactersapp.ui.CharacterViewModel
+import com.anubhav.marvelcharactersapp.util.Resource
+import kotlinx.android.synthetic.main.fragment_character.*
 
 
 class CharacterFragment : Fragment(R.layout.fragment_character) {
     private val TAG = "CharacterFragment"
+    lateinit var viewModel: CharacterViewModel
+    lateinit var characterAdapter: CharacterAdapter
+
     private var _binding : FragmentCharacterBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -19,6 +31,7 @@ class CharacterFragment : Fragment(R.layout.fragment_character) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setupRecyclerView()
         _binding = FragmentCharacterBinding.inflate(inflater,container,false)
         return binding.root
     }
@@ -27,8 +40,29 @@ class CharacterFragment : Fragment(R.layout.fragment_character) {
         super.onViewCreated(view, savedInstanceState)
         with(binding){
 
+            viewModel.characters.observe(viewLifecycleOwner, Observer { response ->
+                when(response) {
+                    is Resource.Success -> {
+                        response.data?.let { characterResponse ->
+                            characterAdapter.differ.submitList(characterResponse.data.results.toList())
+                        }
+                    }
+                    is Resource.Error -> {
+                        response.message?.let { message ->
+                            Log.e(TAG, "An error occured: $message")
+                        }
+                    }
+                }
+            })
         }
     }
 
+    private fun setupRecyclerView(){
+        characterAdapter = CharacterAdapter()
+        rvChar.apply{
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 
 }
